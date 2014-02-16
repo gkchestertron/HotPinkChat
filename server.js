e2996275aa43b6639fecf6de3cb69b5b72edb0fc
gkchesterton <john.fellman@gmail.com>
2014-02-15 22:49:19 -0800

    adds profile

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+var port = Number(process.env.PORT || 5000);
 var http = require("http");
 var path = require("path");
 var mime = require("mime");
@@ -9,4 +10,4 @@ var server = http.createServer(router);
 var chatServer = require('./lib/chat_server.js');
 chatServer.createChat(server);
 
-server.listen();
+server.listen(port);
